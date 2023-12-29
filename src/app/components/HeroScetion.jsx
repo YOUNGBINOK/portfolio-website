cd8ffbcc32117501c3b04359ca570a59fd95cd66
youngbin <redmangos@naver.com>
2023-12-29 16:20:53 +0900

    chage button design

--- src/app/components/HeroScetion.jsx
+++ src/app/components/HeroScetion.jsx
@@ -6,22 +6,26 @@ const HeroScetion = () => {
   return (
     <section>
       <div className="grid grid-cols-1 lg:grid-cols-12">
-        <div className="col-span-7 place-self-center">
+        <div className="col-span-7 place-self-center text-center sm:text-lefts">
           <h1 className="text-white md-4 text-6xl font-extrabold">
             Hello I'm YoungBin
           </h1>
-          <p className="text-[#ADB7BE] text-lg mb-6 lg:text-xl">내용 넣기</p>
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+            내용 넣기
+          </p>
           <div>
-            <button className="px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black">
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-white hover:bg-slate-200 text-black mb-4">
               Hire Me
             </button>
-            <button className="px-6 py-3 rounded-full">Download CV</button>
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full border-solid border-2 border-white">
+              Download CV
+            </button>
           </div>
         </div>
         <div className="col-span-5 place-self-center mt-4 lg:mt-0">
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              src="/images/30_mypic.png"
+              src="/images/30_mypic.pn"
               alt="my Image"
               priority
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
