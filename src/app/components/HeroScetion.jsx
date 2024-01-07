8766e5250c35405ac123e1d4cbb73e89d5bf2398
youngbin <redmangos@naver.com>
2024-01-07 23:27:41 +0900

    add navbar

--- src/app/components/HeroScetion.jsx
+++ src/app/components/HeroScetion.jsx
@@ -1,31 +1,57 @@
+"use client";
 import React from "react";
 // 사진추가
 import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
 
 const HeroScetion = () => {
   return (
-    <section>
+    <section className="overflow-hidden: overflow-hidden">
       <div className="grid grid-cols-1 lg:grid-cols-12">
         <div className="col-span-7 place-self-center text-center sm:text-lefts">
           <h1 className="text-white md-4 text-6xl font-extrabold">
-            Hello I'm YoungBin
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-300">
+              Hello I'm{" "}
+            </span>
+            YoungBin
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                "Web Developer",
+                1000, // wait 1s before replacing "Mice" with "Hamsters"
+                "3D Graphics",
+                1000,
+                "We produce food for Guinea Pigs",
+                1000,
+                "We produce food for Chinchillas",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-            내용 넣기
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Exercitationem dolore, placeat voluptatibus officiis aspernatur
+            recusandae commodi odit ipsa quam ad ab debitis numquam tempore
+            temporibus distinctio labore saepe corrupti sint.
           </p>
           <div>
-            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-white hover:bg-slate-200 text-black mb-4">
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-sky-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white mb-4">
               Hire Me
             </button>
-            <button className="px-6 py-3 w-full sm:w-fit rounded-full border-solid border-2 border-white">
-              Download CV
+            <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500">
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-2 py-2">
+                Download CV
+              </span>
             </button>
           </div>
         </div>
         <div className="col-span-5 place-self-center mt-4 lg:mt-0">
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              src="/images/30_mypic.pn"
+              src="/images/30_mypic.png"
               alt="my Image"
               priority
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import HeroScetion from "./components/HeroScetion";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212] container mx-auto px-12px">
-      <HeroScetion />
+      <Navbar />
+      <div className="container mx-auto px-12 py-4">
+        <HeroScetion />
+      </div>
     </main>
   );
 }
